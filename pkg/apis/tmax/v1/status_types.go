0c68d4af3a96a0282cbcc201018cde5ba3e4871b
JaewonKim <huhu420@naver.com>
2020-06-29 14:36:56 +0900

    [fix] Add DecisionType and define their enum type
    - Add DecisionType and define their enum type DecisionApproved and DecisionRejected
    - Add kubebuilder:validation:Enum to define enum type
    - Add default value of retry as 0
    - Fix typo: erase line which importing error

--- pkg/apis/tmax/v1/status_types.go
+++ pkg/apis/tmax/v1/status_types.go
@@ -3,20 +3,30 @@ package v1
 import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
-	"errors"
 	"time"
+)
+
+// DecisionType field should have Approved or Rejected.
+// +kubebuilder:validation:Enum=Approved;Rejected
+type DecisionType string
+
+const (
+	DecisionApproved DecisionType = "Approved"
+	DecisionRejected DecisionType = "Rejected"
 )
 
 type ApprovalStatus struct {
 	Conditions Conditions `json:"conditions,omitempty" patchStrategy:"merge" patchMergeKey:"type"`
 	Approvers  []Approver `json:"approvers,omitempty"`
-	Retry      int32      `json:"retry"`
+	// +optional
+	// +kubebuilder:default:=0
+	Retry int32 `json:"retry"`
 }
 
 type Approver struct {
-	UserID       string      `json:"userId"`
-	Decision     string      `json:"decision"`
-	ApprovedTime metav1.Time `json:"approvedTime"`
+	UserID       string       `json:"userId"`
+	Decision     DecisionType `json:"decision"`
+	ApprovedTime metav1.Time  `json:"approvedTime"`
 }
 
 func (s *ApprovalStatus) GetCondition(t string) *Condition {
@@ -29,19 +39,21 @@ func (s *ApprovalStatus) GetCondition(t string) *Condition {
 	return nil
 }
 
-func (s *ApprovalStatus) SetApprover(u string) error {
-	for _, appr := range s.Approvers {
-		if appr.UserID == u {
-			return errors.New("duplicated user id")
+func (s *ApprovalStatus) SetApprover(u string, d DecisionType) {
+	for i, _ := range s.Approvers {
+		if s.Approvers[i].UserID == u {
+			s.Approvers[i].Decision = d
+			s.Approvers[i].ApprovedTime = metav1.NewTime(time.Now())
+			return
 		}
 	}
 
 	s.Approvers = append(s.Approvers, Approver{
 		UserID:       u,
+		Decision:     d,
 		ApprovedTime: metav1.NewTime(time.Now()),
 	})
 
-	return nil
 }
 
 func (s *ApprovalStatus) IsApproversOverThreshold(thres int) bool {
